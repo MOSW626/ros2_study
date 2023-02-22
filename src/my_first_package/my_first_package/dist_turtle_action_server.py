49b852225d92484ff70f5c3ddbfe970b2d1ddd3c
MOSW626 <ays6533@naver.com>
2023-02-22 23:31:17 +0900

    modified:   src/my_first_package/my_first_package/dist_turtle_action_server.py

--- src/my_first_package/my_first_package/dist_turtle_action_server.py
+++ src/my_first_package/my_first_package/dist_turtle_action_server.py
@@ -8,6 +8,8 @@ from geometry_msgs.msg import Twist
 from my_first_package_msgs.action import DistTurtle
 from my_first_package.my_subscriber import TurtlesimSubscriber
 
+from rcl_interfaces.msg import SetParametersResult
+
 import math
 import time
 
@@ -30,8 +32,25 @@ class DistTurtleServer(Node):
         self._action_server = ActionServer(self, DistTurtle, 'dist_turtle', self.execute_callback)
         self.declare_parameter('quatile_time', 0.75)
         self.declare_parameter('almost_goal_time', 0.95)
+
         (quantile_time, almosts_time) = self.get_parameters(['quatile_time', 'almost_goal_time'])
-        print('quatile_time and almost_goal time is ', quantile_time.value, almosts_time.value)
+        self.quantile_time = quantile_time.value
+        self.almosts_time = almosts_time.value
+
+        self.add_on_set_parameters_callback(self.parameter_callback)
+
+    def parameter_callback(self, params):
+        for param in params:
+            print(param.name, " is changed to ", param.value)
+
+            if param.name == 'quatile_time':
+                self.quantile_time = param.value
+            if param.name == 'almost_goal_time':
+                self.almosts_time = param.value
+
+        print('quatile_time and almost_goal_time is ', self.quantile_time, self.almosts_time)
+
+        return SetParametersResult(successful=True)
 
     def calc_diff_pose(self):
         if self.is_first_time:
